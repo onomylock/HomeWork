22a397a989ab175ff63dbd7a347625fdd46899bc
onomylock <onomylock@outlook.com>
2022-07-21 18:49:49 +0700

    Готовы вариант

--- HomeWork2/lesson2.c
+++ HomeWork2/lesson2.c
@@ -4,8 +4,6 @@
 
 #define N 10
 #define BUF 20
-
-
 struct Person
 {
     int Number;
@@ -15,24 +13,62 @@ struct Person
 
 void AddPerson(struct Person *ptr)
 {
-    struct Person person1;
+    struct Person person1;    
+    int flag = 0;
+    printf("\nWrite first name:");
+    scanf("%s", person1.FirstName);
+    printf("\nWrite last name:");
+    scanf("%s", person1.LastName);
+    printf("\nWrite number:");
+    scanf("%d", &person1.Number);
 
-    printf("\nWrite first name:%s", person1.FirstName);
-    printf("\nWrite last name:%s", person1.LastName);
-    printf("\nWrite first name:%d", person1.Number);
-
-    // for(int i = 0; i < N; i++)
-    // {
-
-    // }
+    for(int i = 0; i < N; i++)
+    {
+        if(ptr->Number == 0)
+        {
+            for(int j = 0; j < BUF; j++)
+            {
+                ptr->FirstName[j] = person1.FirstName[j];
+                ptr->LastName[j] = person1.LastName[j];
+            }                        
+            ptr->Number = person1.Number;
+            flag = 1;
+            break;
+        }
+        ptr++;
+    }
+    if(flag == 0) printf("Buffer is over!");
 }
 
-void RemovePerson()
-{
+void RemovePerson(struct Person *ptr)
+{    
+    int number;
+    int flag = 0;    
+    char tmp[BUF] = {0};
+    printf("\nWrite person's number:");
+    scanf("%d", &number);
 
+    for(int i = 0; i < N; i++)
+    {
+        if(ptr->Number == number)
+        {
+            //ptr->FirstName = {0};
+            for(int j = 0; j < BUF; j++)
+            {                
+                ptr->FirstName[j] = tmp[j];
+                ptr->LastName[j] = tmp[j];
+            } 
+                                   
+            ptr->Number = 0;
+            flag = 1;
+            break;
+        }
+        ptr++;
+    }
+    if(flag == 0) printf("Number not found");
 }
 
-int FindPersonByFirstName(struct Person *ptr)
+void FindPersonByFirstName(struct Person *ptr)
 {
     char firstName[BUF] = {0};
     printf("\nWrite the first name to search:\n");
@@ -41,14 +77,12 @@ int FindPersonByFirstName(struct Person *ptr)
     {
         if(strcmp(ptr->FirstName, firstName) == 0)
         {
-            printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);
-            return i;
+            printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);            
         }
-    }
-    return -1;
+    }    
 }
 
-int FindPersonByLastName(struct Person *ptr)
+void FindPersonByLastName(struct Person *ptr)
 {
     char lastName[BUF] = {0};
     printf("\nWrite the last name to search:\n");
@@ -57,34 +91,31 @@ int FindPersonByLastName(struct Person *ptr)
     {
         if(strcmp(ptr->LastName, lastName) == 0)
         {
-            printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);
-            return i;
+            printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);            
         }
-    }
-    return -1;
+    }    
 }
 
-int FindPersonByNumber(struct Person *ptr)
+void FindPersonByNumber(struct Person *ptr)
 {
     int number = 0;
     printf("\nWrite the number to search:\n");
     scanf("%d", &number);
     for(int i = 0; i < N; i++)
     {
-        if(ptr->Number = number)
+        if(ptr->Number == number)
         {
-            printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);
-            return i;
+            printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);            
         }
-    }
-    return -1;
+        ptr++;
+    }    
 }
 
 void ViewPersons(struct Person *ptr)
 {
     for(int i = 0; i < N; i++)
     {
-        printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);
+        printf("First Name: %s\t Last Name: %s\t Number: %d\n", ptr->FirstName, ptr->LastName, ptr->Number);        
         ptr++;
     }
 }
@@ -92,21 +123,11 @@ void ViewPersons(struct Person *ptr)
 int main(void)
 {    
     int menuItem;
+    int findItem;
+    char findchars[BUF];
     struct Person persons[N] = {0};
     struct Person *ptr = &persons[0]; 
-
-    // for(int i = 0; i < N; i++)
-    // {
-    //     // for(int j = 0; j < BUF; j++)
-    //     // {
-    //     //     persons[i].FirstName[j] = '_';
-    //     //     persons[i].LastName[j] = '_';
-    //     // }
-    //     persons[i].FirstName[0] = '_';
-    //     persons[i].LastName[0] = '_';        
-    //     persons[i].Number = -1;
-    // }       
-    //printf("%lu", sizeof(persons[0]));
+    
     while (menuItem != 5)
     {
         printf("__________________MENU__________________\n");
@@ -126,22 +147,48 @@ int main(void)
             }            
             case 2:
             {
-                printf("Item2!\n");
+                AddPerson(ptr);
                 break;
             }         
             case 3:
             {
-                printf("Item3!\n");
+                RemovePerson(ptr);
                 break;
             }
             case 4:
             {
-                printf("Item4!\n");
+                printf("\n1) By First Name");
+                printf("\n2) By Last Name");
+                printf("\n3) By Number\n");
+                scanf("%d", &findItem);
+                switch (findItem)
+                {
+                case 1:
+                {                
+                    FindPersonByFirstName(ptr);
+                    break;
+                }
+                case 2:
+                {                
+                    FindPersonByLastName(ptr);
+                    break;
+                }
+                case 3:
+                {                
+                    FindPersonByNumber(ptr);
+                    break;
+                }                   
+                default:
+                {
+                    printf("\nWrong Item!\n");
+                    break;
+                }                    
+                }
                 break;
             }
             case 5:
             {
-                printf("Item5!\n");
+                printf("Good night\n");
                 break;
             }
 
